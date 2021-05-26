6674b95f9177c63c6a75949533f2e786640d32e7
mg <mgidalevich@drivenets.com>
2021-05-26 14:13:33 +0300

    print a real semple rather than mock

--- tsf-client/main.go
+++ tsf-client/main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"drivenets.com/tsf/hal"
 )
 
 func main() {
-	h := hal.NewDnHalMock()
-	var count int
+	//h := hal.NewDnHalMock()
+	//var count int
+	//h.GetInterfaces(
+	//	func(ifc hal.IfName, tm *hal.InterfaceTelemetry) error {
+	//		count += 1
+	//		fmt.Printf("interface: %s\n", ifc)
+	//		fmt.Printf("   speed: %d\n", tm.Speed)
+	//		fmt.Printf("   rx bytes total: %d\n", tm.RxBytes)
+	//		fmt.Printf("   tx bytes total: %d\n", tm.TxBytes)
+	//		fmt.Printf("   rx bytes/sec: %d\n", tm.RxBps)
+	//		fmt.Printf("   tx bytes/sec: %d\n", tm.TxBps)
+	//		fmt.Printf("   delay %.03f\n", tm.Link.Delay)
+	//		fmt.Printf("   jitter %.03f\n", tm.Link.Jitter)
+	//		return nil
+	//	},
+	//)
+	//fmt.Printf("(%d interfaces)\n", count)
+
+	h := hal.NewDnHal()
+
+	time.Sleep(5 * time.Second)
 	h.GetInterfaces(
-		func(ifc hal.IfName, tm *hal.InterfaceTelemetry) error {
-			count += 1
+		func(ifc string, tm *hal.InterfaceTelemetry) error {
 			fmt.Printf("interface: %s\n", ifc)
 			fmt.Printf("   speed: %d\n", tm.Speed)
 			fmt.Printf("   rx bytes total: %d\n", tm.RxBytes)
@@ -21,7 +40,6 @@ func main() {
 			fmt.Printf("   delay %.03f\n", tm.Link.Delay)
 			fmt.Printf("   jitter %.03f\n", tm.Link.Jitter)
 			return nil
-		},
-	)
-	fmt.Printf("(%d interfaces)\n", count)
+		})
+	time.Sleep(5 * time.Minute)
 }
