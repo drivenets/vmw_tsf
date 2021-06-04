232a37991ae3b90e95974ff95a56ae3d77a4eabe
Daniel Badea <dbadea@drivenets.com>
2021-06-04 21:37:47 +0000

    client: utility command line options

--- tsf-client/main.go
+++ tsf-client/main.go
@@ -15,6 +15,7 @@ import (
 
 var monFlowsOpt bool
 var monIfcOpt bool
+var monInterval int
 var flushSteerOpt bool
 var steerFromOpt string
 var steerToOpt string
@@ -22,11 +23,125 @@ var steerProtoOpt string
 var steerNextHopOpt string
 var aclRuleId int
 
-func main() {
+func handleSteer(h hal.DnHal) {
 
+	if steerFromOpt == "" {
+		return
+	}
+
+	if steerToOpt == "" {
+		panic("missing flow destination")
+	}
+
+	if steerNextHopOpt == "" {
+		panic("missint next hop")
+	}
+
+	var err error
+	var port uint64
+
+	fk := &hal.FlowKey{}
+
+	tokens := strings.Split(steerFromOpt, ":")
+	if len(tokens) != 2 {
+		panic("failed to parse flow source")
+	}
+	fk.SrcAddr = net.ParseIP(tokens[0])
+	if port, err = strconv.ParseUint(tokens[1], 10, 16); err != nil {
+		panic("failed to parse flow source port")
+	}
+	fk.SrcPort = uint16(port)
+
+	tokens = strings.Split(steerToOpt, ":")
+	if len(tokens) != 2 {
+		panic("failed to parse flow source")
+	}
+	fk.DstAddr = net.ParseIP(tokens[0])
+	if port, err = strconv.ParseUint(tokens[1], 10, 16); err != nil {
+		panic("failed to parse flow source port")
+	}
+	fk.DstPort = uint16(port)
+
+	switch steerProtoOpt {
+	case "tcp":
+		fk.Protocol = hal.TCP
+	case "udp":
+		fk.Protocol = hal.UDP
+	default:
+		panic("unexpected flow protocol")
+	}
+
+	fmt.Println("steer", fk, "to", steerNextHopOpt, "rule-id", aclRuleId)
+
+	hal.SetAclRuleIndex(aclRuleId)
+	err = h.Steer(fk, steerNextHopOpt)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func monitorInterfaces(h hal.DnHal) {
+	if !monIfcOpt {
+		return
+	}
+	fmt.Println()
+	fmt.Println()
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "Interface\tSpeed\tRx.Bytes/sec\tRx.Bytes/total\tTx.Bytes/sec\tTx.Bytes/total\tDelay\tJitter")
+	fmt.Fprintln(w, "---------\t-----\t------------\t--------------\t------------\t--------------\t-----\t------")
+	count := 0
+	h.GetInterfaces(
+		func(ifc string, tm *hal.InterfaceTelemetry) error {
+			count += 1
+			fmt.Fprintf(w, "%s\t%d\t%d\t%d\t%d\t%d\t%f\t%f\n",
+				ifc, tm.Speed,
+				tm.RxBps, tm.RxBytes,
+				tm.TxBps, tm.TxBytes,
+				tm.Link.Delay, tm.Link.Jitter)
+			return nil
+		})
+	w.Flush()
+	if count == 0 {
+		fmt.Println("(none)")
+	}
+}
+
+func monitorFlows(h hal.DnHal) {
+	if !monFlowsOpt {
+		return
+	}
+	fmt.Println()
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "Source\tDestination\tProto\tIngress\tEgress\tPkt/sec\tPkt/total\tBytes/sec\tBytes/total")
+	fmt.Fprintln(w, "------\t-----------\t-----\t-------\t------\t-------\t---------\t---------\t-----------")
+	count := 0
+	h.GetFlows(
+		func(key *hal.FlowKey, stat *hal.FlowTelemetry) error {
+			count += 1
+			fmt.Fprintf(w, "%s:%d\t%s:%d\t%s\t%s\t%s\t%d\t%d\t%d\t%d\n",
+				key.SrcAddr, key.SrcPort,
+				key.DstAddr, key.DstPort,
+				key.Protocol,
+				stat.IngressIf, stat.EgressIf,
+				stat.RxRatePps, stat.RxTotalPkts,
+				stat.RxRateBps, stat.RxTotalBytes)
+			return nil
+		})
+	w.Flush()
+	if count == 0 {
+		fmt.Println("(none)")
+	}
+}
+
+func main() {
 	flag.BoolVar(&monFlowsOpt, "flows", false, "monitor flows")
 	flag.BoolVar(&monIfcOpt, "interfaces", false, "monitor interfaces")
-	flag.BoolVar(&flushSteerOpt, "flush-steer", false, "flush old steering rules")
+	flag.IntVar(&monInterval, "interval", 10, "monitoring interval")
+	flag.BoolVar(&flushSteerOpt, "flush", false, "flush old steering rules")
 	flag.StringVar(&steerFromOpt, "from", "", "steer flow source ip:port")
 	flag.StringVar(&steerToOpt, "to", "", "steer flow destination ip:port")
 	flag.StringVar(&steerProtoOpt, "proto", "tcp", "steer flow protocol")
@@ -35,103 +150,17 @@ func main() {
 	flag.Parse()
 
 	h := hal.NewDnHal(flushSteerOpt)
-
-	if steerFromOpt != "" {
-
-		if steerToOpt == "" {
-			panic("missing flow destination")
-		}
-		if steerNextHopOpt == "" {
-			panic("missint next hop")
-		}
-
-		var err error
-		var port uint64
-
-		fk := &hal.FlowKey{}
-
-		tokens := strings.Split(steerFromOpt, ":")
-		if len(tokens) != 2 {
-			panic("failed to parse flow source")
-		}
-		fk.SrcAddr = net.ParseIP(tokens[0])
-		if port, err = strconv.ParseUint(tokens[1], 10, 16); err != nil {
-			panic("failed to parse flow source port")
-		}
-		fk.SrcPort = uint16(port)
-
-		tokens = strings.Split(steerToOpt, ":")
-		if len(tokens) != 2 {
-			panic("failed to parse flow source")
-		}
-		fk.DstAddr = net.ParseIP(tokens[0])
-		if port, err = strconv.ParseUint(tokens[1], 10, 16); err != nil {
-			panic("failed to parse flow source port")
-		}
-		fk.DstPort = uint16(port)
-
-		switch steerProtoOpt {
-		case "tcp":
-			fk.Protocol = hal.TCP
-		case "udp":
-			fk.Protocol = hal.UDP
-		default:
-			panic("unexpected flow protocol")
-		}
-
-		fmt.Println("steer", fk, "to", steerNextHopOpt, "rule-id", aclRuleId)
-
-		hal.SetAclRuleIndex(aclRuleId)
-		err = h.Steer(fk, steerNextHopOpt)
-		if err != nil {
-			panic(err)
-		}
-		panic("DONE")
+	handleSteer(h)
+	if !(monFlowsOpt || monIfcOpt) {
+		return
 	}
 
+	os.Setenv("IFC_SAMPLE", strconv.Itoa(monInterval))
 	for {
-		fmt.Println()
-		fmt.Println()
-		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
-		fmt.Fprintln(w, "Interface\tSpeed\tRx.Bytes/sec\tRx.Bytes/total\tTx.Bytes/sec\tTx.Bytes/total\tDelay\tJitter")
-		fmt.Fprintln(w, "---------\t-----\t------------\t--------------\t------------\t--------------\t-----\t------")
-		count := 0
-		h.GetInterfaces(
-			func(ifc string, tm *hal.InterfaceTelemetry) error {
-				count += 1
-				fmt.Fprintf(w, "%s\t%d\t%d\t%d\t%d\t%d\t%f\t%f\n",
-					ifc, tm.Speed,
-					tm.RxBps, tm.RxBytes,
-					tm.TxBps, tm.TxBytes,
-					tm.Link.Delay, tm.Link.Jitter)
-				return nil
-			})
-		w.Flush()
-		if count == 0 {
-			fmt.Println("(none)")
-		}
-
-		fmt.Println()
-		w = tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
-		fmt.Fprintln(w, "Source\tDestination\tProto\tIngress\tEgress\tPkt/sec\tPkt/total\tBytes/sec\tBytes/total")
-		fmt.Fprintln(w, "------\t-----------\t-----\t-------\t------\t-------\t---------\t---------\t-----------")
-		count = 0
-		h.GetFlows(
-			func(key *hal.FlowKey, stat *hal.FlowTelemetry) error {
-				count += 1
-				fmt.Fprintf(w, "%s:%d\t%s:%d\t%s\t%s\t%s\t%d\t%d\t%d\t%d\n",
-					key.SrcAddr, key.SrcPort,
-					key.DstAddr, key.DstPort,
-					key.Protocol,
-					stat.IngressIf, stat.EgressIf,
-					stat.RxRatePps, stat.RxTotalPkts,
-					stat.RxRateBps, stat.RxTotalBytes)
-				return nil
-			})
-		w.Flush()
-		if count == 0 {
-			fmt.Println("(none)")
-		}
-		time.Sleep(10 * time.Second)
+		clearScreen()
+		fmt.Println(time.Now())
+		monitorInterfaces(h)
+		monitorFlows(h)
+		time.Sleep(time.Duration(monInterval) * time.Second)
 	}
 }
