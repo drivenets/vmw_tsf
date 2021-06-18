aaad1ce2ff596c102fc0b235db54a64cb0172b54
Daniel Badea <dbadea@drivenets.com>
2021-06-18 18:09:00 +0000

    hal-client: add batch support
    Add support for collecting and reporting data in json format.
    
    Options:
    - "notw" - do not start TWAMP measurements. Interface's
      reported delay and jitter are zero. TWAMP can still be
      disabled using SKIP_TWAMP=1 environment variable but
      the command line flag is more convenient.
    
    - "batch" - instead of pretty tables and clear screen reported
      collected data in json format.
    
    - "count" - how many times to repeat collecting data. When
      collecting data over an extended period of time it's better
      to use "count" instead of calling hal-client repeatedly to
      avoid time spent in setting up grpc/gnmi clients.
    
    Reported json data can be parsed with "jq". Here's an
    example of how to collect number of flows collected over
    10 seconds with a sampling rate (call to GetFlows) of
    1 second:
    
    /root/hal-client \
        -notw -batch -flows \
        -count 10 -interval 1 \
      | jq '.[]|[.time, .flows|length]| @csv'

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
@@ -12,6 +13,7 @@ import (
 	"time"
 
 	hal "github.com/drivenets/vmw_tsf/pkg/hal"
+	"github.com/prometheus/common/log"
 )
 
 var monFlowsOpt bool
@@ -24,6 +26,9 @@ var steerProtoOpt string
 var steerNextHopOpt string
 var aclRuleId int
 var noClsOpt bool
+var noTwampOpt bool
+var batchOpt bool
+var countOpt int
 
 func handleSteer(h hal.DnHal) {
 
@@ -73,11 +78,11 @@ func handleSteer(h hal.DnHal) {
 	if steerNextHopOpt == "" {
 		err = h.RemoveSteer([]hal.FlowKey{fk})
 	} else {
-		fmt.Println("steer", fk, "to", steerNextHopOpt)
+		log.Info("steer", fk, "to", steerNextHopOpt)
 		steerItem := hal.SteerItem{Rule: &fk, NextHop: steerNextHopOpt}
 		err = h.Steer([]hal.SteerItem{steerItem})
-		fmt.Println("verifying rule ", fk, "to", steerNextHopOpt, "has been applied")
-		fmt.Println(h.GetSteerInterface([]hal.SteerItem{steerItem}))
+		log.Info("verifying rule ", fk, "to", steerNextHopOpt, "has been applied")
+		log.Infoln(h.GetSteerInterface([]hal.SteerItem{steerItem}))
 	}
 	if err != nil {
 		panic(err)
@@ -116,9 +121,6 @@ func clearScreen() {
 }
 
 func printInterfaces(h hal.DnHal) {
-	if !monIfcOpt {
-		return
-	}
 	fmt.Println()
 	fmt.Println()
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
@@ -146,9 +148,6 @@ func printInterfaces(h hal.DnHal) {
 }
 
 func printFlows(h hal.DnHal) {
-	if !monFlowsOpt {
-		return
-	}
 	fmt.Println()
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
 	fmt.Fprintln(w, "Source\tDestination\tProto\tIngress\tEgress\tPkt/sec\tPkt/total\tBytes/sec\tBytes/total")
@@ -176,6 +175,114 @@ func printFlows(h hal.DnHal) {
 	}
 }
 
+func monitor(h hal.DnHal) {
+	for {
+		clearScreen()
+		fmt.Println(time.Now())
+		if monIfcOpt {
+			printInterfaces(h)
+		}
+		if monFlowsOpt {
+			printFlows(h)
+		}
+		time.Sleep(time.Duration(monInterval) * time.Second)
+	}
+}
+
+type BatchInterfaceOutput struct {
+	Name    string  `json:"name"`
+	Type    string  `json:"type"`
+	Speed   uint64  `json:"speed"`
+	RxBytes uint64  `json:"rx-bytes"`
+	RxBps   uint64  `json:"rx-bps"`
+	TxBytes uint64  `json:"tx-bytes"`
+	TxBps   uint64  `json:"tx-bps"`
+	Delay   float64 `json:"delay"`
+	Jitter  float64 `json:"jitter"`
+}
+
+type BatchFlowOutput struct {
+	Source      string `json:"source"`
+	Destination string `json:"destination"`
+	Protocol    string `json:"protocol"`
+	Ingress     string `json:"ingress"`
+	Egress      string `json:"egress"`
+	RxPps       uint64 `json:"rx-pps"`
+	RxPkts      uint64 `json:"rx-packets"`
+	RxBps       uint64 `json:"rx-bps"`
+	RxBytes     uint64 `json:"rx-bytes"`
+}
+
+type BatchOutput struct {
+	Time       float64                `json:"time"`
+	Interfaces []BatchInterfaceOutput `json:"interfaces"`
+	Flows      []BatchFlowOutput      `json:"flows"`
+}
+
+func jsonInterfaces(h hal.DnHal, b *BatchOutput) {
+	ifType := "n/a"
+	getIfStats := func(ifc string, tm *hal.InterfaceTelemetry) error {
+		b.Interfaces = append(b.Interfaces, BatchInterfaceOutput{
+			Name:    ifc,
+			Type:    ifType,
+			Speed:   tm.Speed,
+			RxBps:   tm.RxBps,
+			RxBytes: tm.RxBytes,
+			TxBps:   tm.TxBps,
+			TxBytes: tm.TxBytes,
+			Delay:   tm.Link.Delay,
+			Jitter:  tm.Link.Jitter,
+		})
+		return nil
+	}
+	ifType = "lan"
+	h.GetLanInterfaces(getIfStats)
+	ifType = "wan"
+	h.GetWanInterfaces(getIfStats)
+}
+
+func jsonFlows(h hal.DnHal, b *BatchOutput) {
+	h.GetFlows(
+		func(key *hal.FlowKey, stat *hal.FlowTelemetry) error {
+			b.Flows = append(b.Flows, BatchFlowOutput{
+				Source:      fmt.Sprintf("%s:%d", key.SrcAddr, key.SrcPort),
+				Destination: fmt.Sprintf("%s:%d", key.DstAddr, key.DstPort),
+				Protocol:    key.Protocol.String(),
+				Ingress:     stat.IngressIf,
+				Egress:      stat.EgressIf,
+				RxPps:       stat.RxRatePps,
+				RxPkts:      stat.RxTotalPkts,
+				RxBps:       stat.RxRateBps,
+				RxBytes:     stat.RxTotalBytes,
+			})
+			return nil
+		})
+}
+
+func batch(h hal.DnHal) {
+	batch := make([]BatchOutput, countOpt)
+	start := time.Now()
+	for idx := 0; idx < countOpt; idx++ {
+		time.Sleep(time.Duration(monInterval) * time.Second)
+		entry := BatchOutput{
+			Time:       time.Since(start).Seconds(),
+			Interfaces: make([]BatchInterfaceOutput, 0, 3),
+			Flows:      make([]BatchFlowOutput, 0, 1000)}
+		if monIfcOpt {
+			jsonInterfaces(h, &entry)
+		}
+		if monFlowsOpt {
+			jsonFlows(h, &entry)
+		}
+		batch[idx] = entry
+	}
+	out, err := json.Marshal(batch)
+	if err != nil {
+		log.Error("Failed to marshal json output. Reason:", err)
+	}
+	fmt.Printf("%s\n", out)
+}
+
 func main() {
 	flag.BoolVar(&monFlowsOpt, "flows", false, "monitor flows")
 	flag.BoolVar(&monIfcOpt, "interfaces", false, "monitor interfaces")
@@ -187,8 +294,14 @@ func main() {
 	flag.StringVar(&steerNextHopOpt, "next-hop", "", "steer to next-hop")
 	flag.BoolVar(&noClsOpt, "nocls", false, "do not clear screen")
 	flag.IntVar(&aclRuleId, "id", 10, "steer rule id")
+	flag.BoolVar(&noTwampOpt, "notw", false, "do not start twamp measurements")
+	flag.BoolVar(&batchOpt, "batch", false, "batch mode exit after first interval")
+	flag.IntVar(&countOpt, "count", 1, "how many batch rounds to execute")
 	flag.Parse()
 
+	if noTwampOpt {
+		os.Setenv("SKIP_TWAMP", "1")
+	}
 	if !(monFlowsOpt || monIfcOpt) {
 		os.Setenv("SKIP_TWAMP", "1")
 	}
@@ -205,11 +318,10 @@ func main() {
 		return
 	}
 
-	for {
-		clearScreen()
-		fmt.Println(time.Now())
-		printInterfaces(h)
-		printFlows(h)
-		time.Sleep(time.Duration(monInterval) * time.Second)
+	if batchOpt {
+		batch(h)
+	} else {
+		monitor(h)
 	}
+
 }
