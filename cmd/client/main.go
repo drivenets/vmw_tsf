6585d3071357af7926e7f71f4405b184d30c5bc8
Daniel Badea <dbadea@drivenets.com>
2021-06-23 14:57:41 +0000

    hal-client: support accumulated statistics
    Display accumulated statistics in batch mode:
    1. interface statistics are averaged over time
    2. all flows are stored and statistics updated
       every collect interval even for flows that
       are not present in the current GetFlows()
       result

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -29,6 +29,7 @@ var noClsOpt bool
 var noTwampOpt bool
 var batchOpt bool
 var countOpt int
+var accOpt bool
 
 func handleSteer(h hal.DnHal) {
 
@@ -190,27 +191,29 @@ func monitor(h hal.DnHal) {
 }
 
 type BatchInterfaceOutput struct {
-	Name    string  `json:"name"`
-	Type    string  `json:"type"`
-	Speed   uint64  `json:"speed"`
-	RxBytes uint64  `json:"rx-bytes"`
-	RxBps   uint64  `json:"rx-bps"`
-	TxBytes uint64  `json:"tx-bytes"`
-	TxBps   uint64  `json:"tx-bps"`
-	Delay   float64 `json:"delay"`
-	Jitter  float64 `json:"jitter"`
+	Name    string        `json:"name"`
+	Type    string        `json:"type"`
+	Speed   uint64        `json:"speed"`
+	RxBytes uint64        `json:"rx-bytes"`
+	RxBps   uint64        `json:"rx-bps"`
+	TxBytes uint64        `json:"tx-bytes"`
+	TxBps   uint64        `json:"tx-bps"`
+	Delay   float64       `json:"delay"`
+	Jitter  float64       `json:"jitter"`
+	Time    time.Duration `json:"-"` // accumulated time
 }
 
 type BatchFlowOutput struct {
-	Source      string `json:"source"`
-	Destination string `json:"destination"`
-	Protocol    string `json:"protocol"`
-	Ingress     string `json:"ingress"`
-	Egress      string `json:"egress"`
-	RxPps       uint64 `json:"rx-pps"`
-	RxPkts      uint64 `json:"rx-packets"`
-	RxBps       uint64 `json:"rx-bps"`
-	RxBytes     uint64 `json:"rx-bytes"`
+	Source      string        `json:"source"`
+	Destination string        `json:"destination"`
+	Protocol    string        `json:"protocol"`
+	Ingress     string        `json:"ingress"`
+	Egress      string        `json:"egress"`
+	RxPps       uint64        `json:"rx-pps"`
+	RxPkts      uint64        `json:"rx-packets"`
+	RxBps       uint64        `json:"rx-bps"`
+	RxBytes     uint64        `json:"rx-bytes"`
+	Time        time.Duration `json:"-"` // accumulated time
 }
 
 type BatchOutput struct {
@@ -219,7 +222,7 @@ type BatchOutput struct {
 	Flows      []BatchFlowOutput      `json:"flows"`
 }
 
-func jsonInterfaces(h hal.DnHal, b *BatchOutput) {
+func jsonInterfaces(h hal.DnHal, b *BatchOutput, t time.Duration) {
 	ifType := "n/a"
 	getIfStats := func(ifc string, tm *hal.InterfaceTelemetry) error {
 		b.Interfaces = append(b.Interfaces, BatchInterfaceOutput{
@@ -232,6 +235,7 @@ func jsonInterfaces(h hal.DnHal, b *BatchOutput) {
 			TxBytes: tm.TxBytes,
 			Delay:   tm.Link.Delay,
 			Jitter:  tm.Link.Jitter,
+			Time:    t,
 		})
 		return nil
 	}
@@ -241,7 +245,7 @@ func jsonInterfaces(h hal.DnHal, b *BatchOutput) {
 	h.GetWanInterfaces(getIfStats)
 }
 
-func jsonFlows(h hal.DnHal, b *BatchOutput) {
+func jsonFlows(h hal.DnHal, b *BatchOutput, t time.Duration) {
 	h.GetFlows(
 		func(key *hal.FlowKey, stat *hal.FlowTelemetry) error {
 			b.Flows = append(b.Flows, BatchFlowOutput{
@@ -254,33 +258,203 @@ func jsonFlows(h hal.DnHal, b *BatchOutput) {
 				RxPkts:      stat.RxTotalPkts,
 				RxBps:       stat.RxRateBps,
 				RxBytes:     stat.RxTotalBytes,
+				Time:        t,
 			})
 			return nil
 		})
 }
 
+type AccumulatedOutput struct {
+	Time       float64                         `json:"time"`
+	Interfaces map[string]BatchInterfaceOutput `json:"interfaces"`
+	Flows      map[string]BatchFlowOutput      `json:"flows"`
+}
+
+func NewAccumulatedOutput() AccumulatedOutput {
+	acc := AccumulatedOutput{}
+	acc.Interfaces = make(map[string]BatchInterfaceOutput)
+	acc.Flows = make(map[string]BatchFlowOutput)
+	return acc
+}
+
+func (acc *AccumulatedOutput) Copy() AccumulatedOutput {
+	cp := NewAccumulatedOutput()
+	cp.Time = acc.Time
+	for key, val := range acc.Interfaces {
+		cp.Interfaces[key] = val
+	}
+	for key, val := range acc.Flows {
+		cp.Flows[key] = val
+	}
+	return cp
+}
+
+type AverageF64 struct {
+	weightA float64
+	weightB float64
+}
+
+func MakeAverageF64(wa float64, wb float64) AverageF64 {
+	return AverageF64{
+		weightA: wa,
+		weightB: wb,
+	}
+}
+
+func (avg AverageF64) F64(a float64, b float64) float64 {
+	return (a*avg.weightA + b*avg.weightB) / (avg.weightA + avg.weightB)
+}
+
+func (avg AverageF64) U64(a uint64, b uint64) uint64 {
+	return uint64((float64(a)*avg.weightA + float64(b)*avg.weightB) / (avg.weightA + avg.weightB))
+}
+
+func updateAccInterfaces(acc AccumulatedOutput, entry *BatchOutput, interval time.Duration, next AccumulatedOutput) {
+	for _, ifc := range entry.Interfaces {
+		if accIf, found := acc.Interfaces[ifc.Name]; found {
+			// Update existing interfaces
+			nextIf := BatchInterfaceOutput{
+				Name: ifc.Name,
+				Type: ifc.Type,
+			}
+
+			avg := MakeAverageF64(accIf.Time.Seconds(), ifc.Time.Seconds())
+			nextIf.Speed = avg.U64(accIf.Speed, ifc.Speed)
+			nextIf.RxBps = avg.U64(accIf.RxBps, ifc.RxBps)
+			nextIf.RxBytes = avg.U64(accIf.RxBytes, ifc.RxBytes)
+			nextIf.TxBps = avg.U64(accIf.TxBps, ifc.TxBps)
+			nextIf.TxBytes = avg.U64(accIf.TxBytes, ifc.TxBytes)
+			nextIf.Delay = avg.F64(accIf.Delay, ifc.Delay)
+			nextIf.Jitter = avg.F64(accIf.Jitter, ifc.Jitter)
+
+			next.Interfaces[nextIf.Name] = nextIf
+		} else {
+			// Track new interfaces
+			next.Interfaces[ifc.Name] = ifc
+		}
+	}
+	for _, accIf := range acc.Interfaces {
+		if _, found := next.Interfaces[accIf.Name]; !found {
+			// Update average of interfaces not in current update
+			nextIf := BatchInterfaceOutput{
+				Name: accIf.Name,
+				Type: accIf.Type,
+			}
+			avg := MakeAverageF64(accIf.Time.Seconds(), interval.Seconds())
+			nextIf.Speed = avg.U64(accIf.Speed, 0)
+			nextIf.RxBps = avg.U64(accIf.RxBps, 0)
+			nextIf.RxBytes = avg.U64(accIf.RxBytes, 0)
+			nextIf.TxBps = avg.U64(accIf.TxBps, 0)
+			nextIf.TxBytes = avg.U64(accIf.TxBytes, 0)
+			nextIf.Delay = avg.F64(accIf.Delay, 0)
+			nextIf.Jitter = avg.F64(accIf.Jitter, 0)
+
+			next.Interfaces[nextIf.Name] = nextIf
+		}
+	}
+}
+
+func (fo *BatchFlowOutput) Key() string {
+	return fmt.Sprintf("%s:%s:%s", fo.Source, fo.Destination, fo.Protocol)
+}
+
+func updateAccFlows(acc AccumulatedOutput, entry *BatchOutput, interval time.Duration, next AccumulatedOutput) {
+	for _, flow := range entry.Flows {
+		key := flow.Key()
+		if accFlow, found := acc.Flows[key]; found {
+			// Update existing flows
+			nextFlow := BatchFlowOutput{
+				Source:      flow.Source,
+				Destination: flow.Destination,
+				Protocol:    flow.Protocol,
+				Ingress:     flow.Ingress,
+				Egress:      flow.Egress,
+			}
+
+			avg := MakeAverageF64(accFlow.Time.Seconds(), flow.Time.Seconds())
+			nextFlow.RxPps = avg.U64(accFlow.RxPps, flow.RxPps)
+			nextFlow.RxPkts = avg.U64(accFlow.RxPkts, flow.RxPkts)
+			nextFlow.RxBps = avg.U64(accFlow.RxBps, flow.RxBps)
+			nextFlow.RxBytes = avg.U64(accFlow.RxBytes, flow.RxBytes)
+			next.Flows[key] = nextFlow
+		} else {
+			// Track new flows
+			next.Flows[key] = flow
+		}
+	}
+	for _, accFlow := range acc.Flows {
+		key := accFlow.Key()
+		if _, found := next.Flows[key]; !found {
+			// Update average of flows not in current update
+			nextFlow := BatchFlowOutput{
+				Source:      accFlow.Source,
+				Destination: accFlow.Destination,
+				Protocol:    accFlow.Protocol,
+				Ingress:     accFlow.Ingress,
+				Egress:      accFlow.Egress,
+			}
+			avg := MakeAverageF64(accFlow.Time.Seconds(), interval.Seconds())
+			nextFlow.RxPps = avg.U64(accFlow.RxPps, 0)
+			nextFlow.RxPkts = avg.U64(accFlow.RxPkts, 0)
+			nextFlow.RxBps = avg.U64(accFlow.RxBps, 0)
+			nextFlow.RxBytes = avg.U64(accFlow.RxBytes, 0)
+
+			next.Flows[key] = nextFlow
+		}
+	}
+}
+
+func updateAccOutput(acc AccumulatedOutput, entry *BatchOutput, interval time.Duration) AccumulatedOutput {
+	next := NewAccumulatedOutput()
+	updateAccInterfaces(acc, entry, interval, next)
+	updateAccFlows(acc, entry, interval, next)
+	return next
+}
+
 func batch(h hal.DnHal) {
 	batch := make([]BatchOutput, countOpt)
+	accLog := make([]AccumulatedOutput, countOpt)
+	acc := NewAccumulatedOutput()
 	start := time.Now()
+	prev := time.Now()
 	for idx := 0; idx < countOpt; idx++ {
 		time.Sleep(time.Duration(monInterval) * time.Second)
+		elapsed := time.Since(start)
+		tick := time.Since(prev)
+		prev = time.Now()
+
 		entry := BatchOutput{
-			Time:       time.Since(start).Seconds(),
+			Time:       elapsed.Seconds(),
 			Interfaces: make([]BatchInterfaceOutput, 0, 3),
 			Flows:      make([]BatchFlowOutput, 0, 1000)}
+		prev = time.Now()
 		if monIfcOpt {
-			jsonInterfaces(h, &entry)
+			jsonInterfaces(h, &entry, tick)
 		}
 		if monFlowsOpt {
-			jsonFlows(h, &entry)
+			jsonFlows(h, &entry, tick)
 		}
 		batch[idx] = entry
+		if accOpt {
+			acc = updateAccOutput(acc, &entry, tick)
+			acc.Time = elapsed.Seconds()
+			accLog[idx] = acc.Copy()
+		}
 	}
-	out, err := json.Marshal(batch)
-	if err != nil {
-		log.Error("Failed to marshal json output. Reason:", err)
+
+	if accOpt {
+		if out, err := json.Marshal(accLog); err != nil {
+			log.Error("Failed to marshal json output. Reason:", err)
+		} else {
+			fmt.Printf("%s\n", out)
+		}
+	} else {
+		if out, err := json.Marshal(batch); err != nil {
+			log.Error("Failed to marshal json output. Reason:", err)
+		} else {
+			fmt.Printf("%s\n", out)
+		}
 	}
-	fmt.Printf("%s\n", out)
 }
 
 func main() {
@@ -297,6 +471,7 @@ func main() {
 	flag.BoolVar(&noTwampOpt, "notw", false, "do not start twamp measurements")
 	flag.BoolVar(&batchOpt, "batch", false, "batch mode exit after first interval")
 	flag.IntVar(&countOpt, "count", 1, "how many batch rounds to execute")
+	flag.BoolVar(&accOpt, "accumulate", false, "accumulate flow statistics in batch mode")
 	flag.Parse()
 
 	if noTwampOpt {
@@ -316,6 +491,10 @@ func main() {
 	handleSteer(h)
 	if !(monFlowsOpt || monIfcOpt) {
 		return
+	}
+
+	if accOpt {
+		batchOpt = true
 	}
 
 	if batchOpt {
